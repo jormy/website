7ed1b1b640f89ce49f285444044307bce9af5dfe
jorm <charkuayteow.10@gmail.com>
2024-07-13 13:00:03 +0800

    automatic cursor timeout + scale on mousedown

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,30 +1,56 @@
-import React from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
 
 export const Cursor = () => {
-
-// TODO: Add scaling on click, timeout for opacity
-
-    let opacity = 1;
-    let scale = 1;
+    const cursorRef = useRef(null);
+    const [isMouseDown, setIsMouseDown] = useState(false);
     let timeout;
 
+    const resetOpacity = () => {
+        clearTimeout(timeout);
+        cursorRef.current.style.opacity = '1';
+        timeout = setTimeout(() => {
+            cursorRef.current.style.opacity = '0';
+        }, 1000);
+    };
 
-    const cursorRef = React.useRef(null);
-
-    React.useEffect(() => {
-        document.addEventListener('mousemove', (e) => {
-            const {clientX, clientY} = e;
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            const { clientX, clientY } = e;
             const mouseX = clientX - cursorRef.current.clientWidth / 2;
             const mouseY = clientY - cursorRef.current.clientHeight / 2;
-            cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-        });
-    }, [])
+            cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0) scale(${isMouseDown ? 1.25 : 1})`;
+            resetOpacity();
+        };
 
+        const handleMouseDown = () => {
+            setIsMouseDown(true);
+            cursorRef.current.style.transform += ' scale(1.75)';
+        };
+
+        const handleMouseUp = () => {
+            setIsMouseDown(false);
+            cursorRef.current.style.transform = cursorRef.current.style.transform.replace(' scale(1.25)', '');
+        };
+
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mousedown', handleMouseDown);
+        document.addEventListener('mouseup', handleMouseUp);
+
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mousedown', handleMouseDown);
+            document.removeEventListener('mouseup', handleMouseUp);
+            clearTimeout(timeout);
+        };
+    }, [isMouseDown]);
 
     return (
-        <div className="left-0 top-0 w-7 h-7 border-solid border-[1px] rounded-full border-blue-body fixed z-50 pointer-events-none cursor-transtions" ref={cursorRef}></div>
+        <div
+            className="left-0 top-0 w-7 h-7 border-solid border-[1px] rounded-full border-blue-body fixed z-50 pointer-events-none cursor-transitions"
+            ref={cursorRef}
+            style={{ opacity: 1 }}
+        ></div>
     );
-}
+};
 
 export default Cursor;

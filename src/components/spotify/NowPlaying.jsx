2c5c6099a5b2f6a71933185e057f47b5eede7e2a
jorm <charkuayteow.10@gmail.com>
2024-07-30 11:52:46 +0800

    backend :D

--- src/components/spotify/NowPlaying.jsx
+++ src/components/spotify/NowPlaying.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import getNowPlayingItem, { getAccessToken } from "./SpotifyAPI";
+import getNowPlayingItem from "./SpotifyAPI";
 import TruncateString from "../../components/spotify/TruncateString";
 import visualiser from "../../assets/visualiser.gif";
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
@@ -8,73 +8,71 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const POLLING_INTERVAL = 10000; // fetch song every 10 seconds
 
 export const NowPlaying = (props) => {
-    const [loading, setLoading] = useState(true);
-    const [result, setResult] = useState({});
-    const [prevSong, setPrevSong] = useState({});
-    const [accessTokenData, setAccessTokenData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [result, setResult] = useState({});
+  const [prevSong, setPrevSong] = useState({});
 
-    const fetchNowPlaying = async () => {
-        let tokenData = accessTokenData;
+  const fetchNowPlaying = async () => {
+    const nowPlaying = await getNowPlayingItem();
 
-        // Check if token is expired or not available
-        if (!tokenData || Date.now() > tokenData.expiry_time) {
-            tokenData = await getAccessToken();
-            setAccessTokenData(tokenData);
-        }
+    // Update the state only if the song has changed
+    if (nowPlaying && nowPlaying.songUrl !== prevSong.songUrl) {
+      setResult(nowPlaying);
+      setPrevSong(nowPlaying);
+      setLoading(false);
+    } else if (!nowPlaying) {
+      setResult({});
+      setPrevSong({});
+      setLoading(false);
+    }
+  };
 
-        const nowPlaying = await getNowPlayingItem(tokenData.access_token);
+  useEffect(() => {
+    fetchNowPlaying(); // Initial fetch
 
-        // Update the state only if the song has changed
-        if (nowPlaying && nowPlaying.songUrl !== prevSong.songUrl) {
-            setResult(nowPlaying);
-            setPrevSong(nowPlaying);
-            setLoading(false);
-        } else if (!nowPlaying) {
-            setResult({});
-            setPrevSong({});
-            setLoading(false);
-        }
-    };
+    const interval = setInterval(() => {
+      fetchNowPlaying();
+    }, POLLING_INTERVAL);
 
-    useEffect(() => {
-        fetchNowPlaying(); // Initial fetch
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, []);
 
-        const interval = setInterval(() => {
-            fetchNowPlaying();
-        }, POLLING_INTERVAL);
-
-        return () => clearInterval(interval); // Cleanup interval on component unmount
-    }, [props.client_id, props.client_secret, props.refresh_token, accessTokenData]);
-
-    return (
-        <div className="flex w-fit text-denim-300">
-            {loading && <p>Loading...</p>}
-            {!loading && !result.isPlaying && (
-                <>
-                    <FontAwesomeIcon icon={faSpotify} className="w-12 h-12"/>
-                    <span className="pl-3 text-md">Not playing <br/> anything</span>
-                </>
-            )}
-            {!loading && result.isPlaying && (
-                <>
-                    <img 
-                        src={result.albumImageUrl} 
-                        alt={`${result.title} album cover`} 
-                        className="w-12 h-12 rounded-md"
-                    />
-                    <div className="pl-3">
-                        <div className="flex relative">
-                            <a href={result.songUrl} target="_blank" rel="noopener noreferrer" className="font-bold text-denim-200 text-md pr-6 flex gap-3">
-                                {TruncateString(result.title, 40)}
-                                <img src={visualiser} className="h-[1em] w-[1-em]"/>
-                            </a>
-                        </div>
-                        <p>{result.artist}</p>
-                    </div>
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div className="flex w-fit text-denim-300">
+      {loading && <p>Loading...</p>}
+      {!loading && !result.isPlaying && (
+        <>
+          <FontAwesomeIcon icon={faSpotify} className="w-12 h-12" />
+          <span className="pl-3 text-md">
+            Not playing <br /> anything
+          </span>
+        </>
+      )}
+      {!loading && result.isPlaying && (
+        <>
+          <img
+            src={result.albumImageUrl}
+            alt={`${result.title} album cover`}
+            className="w-12 h-12 rounded-md"
+          />
+          <div className="pl-3">
+            <div className="flex relative">
+              <a
+                href={result.songUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold text-denim-200 text-md pr-6 flex gap-3"
+              >
+                {TruncateString(result.title, 40)}
+                <img src={visualiser} className="h-[1em] w-[1-em]" />
+              </a>
+            </div>
+            <p>{result.artist}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default NowPlaying;
--- src/components/spotify/SpotifyAPI.jsx
+++ src/components/spotify/SpotifyAPI.jsx
@@ -1,59 +1,24 @@
-import querystring from "querystring";
+const SPOTIFY_ENDPOINT = `http://jorm-server.vercel.app/spotify`;
 
-const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
-const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+export default async function getNowPlayingItem() {
+  try {
+    const response = await fetch(SPOTIFY_ENDPOINT);
 
-const client_id = import.meta.env.VITE_CLIENT_ID;
-const client_secret = import.meta.env.VITE_CLIENT_SECRET;
-const refresh_token = import.meta.env.VITE_REFRESH_TOKEN;
+    if (response.status === 204 || response.status > 400) {
+      return false;
+    }
 
-export const getAccessToken = async () => {
-  const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+    const song = await response.json();
 
-  const response = await fetch(TOKEN_ENDPOINT, {
-    method: "POST",
-    headers: {
-      Authorization: `Basic ${basic}`,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: querystring.stringify({
-      grant_type: "refresh_token",
-      refresh_token,
-    }),
-  });
-
-  const data = await response.json();
-  data.expiry_time = Date.now() + data.expires_in * 1000; // Calculate expiry time
-
-  return data;
-};
-
-export const getNowPlaying = async (access_token) => {
-  return fetch(NOW_PLAYING_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
-};
-
-export default async function getNowPlayingItem(access_token) {
-  const response = await getNowPlaying(access_token);
-  if (response.status === 204 || response.status > 400) {
+    return {
+      albumImageUrl: song.albumImageUrl,
+      artist: song.artist,
+      isPlaying: song.isPlaying,
+      songUrl: song.songUrl,
+      title: song.title,
+    };
+  } catch (error) {
+    console.error("Error fetching the song information:", error);
     return false;
   }
-
-  const song = await response.json();
-  const albumImageUrl = song.item.album.images[0].url;
-  const artist = song.item.artists.map((_artist) => _artist.name).join(", ");
-  const isPlaying = song.is_playing;
-  const songUrl = song.item.external_urls.spotify;
-  const title = song.item.name;
-
-  return {
-    albumImageUrl,
-    artist,
-    isPlaying,
-    songUrl,
-    title,
-  };
 }

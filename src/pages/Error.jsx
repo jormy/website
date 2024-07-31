a85c7fdcb41a71519186c74c0624152ddba2d02e
jorm <charkuayteow.10@gmail.com>
2024-07-31 11:21:20 +0800

    404 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
+
 function Error() {
-    return <h1>404 not found :3</h1>
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <div className="items-center flex flex-col">
+          <h1 className="font-header text-denim-200 text-9xl">404</h1>
+          <p className="text-xl mt-5 mb-7">the page requested was not found</p>
+          <span>
+            <Link
+              to="/"
+              className="text-xl py-2 px-4 rounded-full bg-denim-300/[0.1] hover:bg-denim-300/[0.2] transition ease-in"
+            >
+              go back home <IoIosArrowForward className="inline" />
+            </Link>
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Error;

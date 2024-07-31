d8792ce30b3e5b59b11534a1c512ae159575a697
jorm <charkuayteow.10@gmail.com>
2024-07-31 18:32:25 +0800

    cleaner code

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,6 +2,27 @@ import { Link } from "react-router-dom";
 import ProjectCard from "./ProjectCard";
 
 function Projects() {
+  const projects = [
+    {
+      name: "song-search",
+      descr: "a basic song search app using react",
+      link: "https://songsearch.vercel.app/",
+      repo: "https://github.com/jormy/song-search",
+    },
+    {
+      name: "website-v2",
+      descr:
+        "attempt at creating a website using reactjs. I hate js so idk why i did this",
+      link: "https://jorm.vercel.app/",
+    },
+    {
+      name: "old-website",
+      descr:
+        "First site i made using html and css. Never got around to finishing it.",
+      link: "https://jormy.github.io/",
+    },
+  ];
+
   return (
     <>
       <div className="flex items-center mb-2">
@@ -14,29 +35,15 @@ function Projects() {
         <div className="ml-3 border-b-[1px] flex-grow border-denim-300/[0.5]"></div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <ProjectCard
-          name="website-v2"
-          descr="attempt at creating a website using reactjs. I hate js so idk why i did this"
-          link="https://github.com/jormy/website-v2"
-        />
-
-        <ProjectCard
-          name="old-website"
-          descr="My first site ever (as you can probably tell from the code)"
-          link="https://github.com/jormy/jormy.github.io"
-        />
-
-        <ProjectCard
-          name="future-project"
-          descr="ill add more projects here as i make them"
-          link="https://github.com/jormy"
-        />
-
-        <ProjectCard
-          name="future-project"
-          descr="i swear ill do something soon i promise"
-          link="https://github.com/jormy"
-        />
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.name}
+            name={project.name}
+            descr={project.descr}
+            link={project.link}
+            repo={project.repo}
+          />
+        ))}
       </div>
     </>
   );

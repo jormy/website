63b539e3fd604187027fb9a2c38985d491c80494
jorm <charkuayteow.10@gmail.com>
2024-07-31 10:54:56 +0800

    feat: active navlinks

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,22 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import NowPlaying from "../spotify/NowPlaying";
-import LinkWrapper from "../navbar/LinkWrapper";
+import NavLinkWrapper from "./NavLinkWrapper";
 
 //TODO: replace links with hamburger menu for mobile
 const Navbar = () => {
   return (
     <>
       <div className="flex justify-between max-w-4xl ">
-        <nav className="flex-1 flex gap-5 text-denim-200 text-lg">
-          <Link to="/">
-            <LinkWrapper title="/" />
-          </Link>
-          <Link to="/projects">
-            <LinkWrapper title="projects" />
-          </Link>
-          <Link to="/contact">
-            <LinkWrapper title="contact" />
-          </Link>
+        <nav className="flex-1 flex gap-5 text-denim-300 text-lg">
+          <NavLink to="/">
+            <NavLinkWrapper title="/" />
+          </NavLink>
+          <NavLink to="/projects">
+            <NavLinkWrapper title="projects" />
+          </NavLink>
+          <NavLink to="/contact">
+            <NavLinkWrapper title="contact" />
+          </NavLink>
         </nav>
         <NowPlaying />
       </div>
